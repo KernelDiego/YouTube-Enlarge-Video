672e79df2a371f35362f8da6e4027c004298e4bf
DiegoKernel <gonzalezdiego.contact@gmail.com>
2022-12-29 00:51:04 -0300

    Delete archives

--- content.js
+++
@@ -1,3 +0,0 @@
-chrome.storage.sync.get('cursor', ({cursor}) => {
-    document.body.style.cursor = 'url("https://raw.githubusercontent.com/trecno/browser-addon/main/images/cursor_' + cursor + '.png"), auto';
-});
